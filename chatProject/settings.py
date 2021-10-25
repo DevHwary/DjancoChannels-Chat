971759c8b6f9afc7bb6ad3ecd6d3c27b297d303b
DevHwary <mh.hwary@yahoo.com>
2021-10-26 01:42:52 +0200

    add the channel layer, group chat, end of part_2

--- chatProject/settings.py
+++ chatProject/settings.py
@@ -73,6 +73,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'chatProject.wsgi.application'
 ASGI_APPLICATION = 'chatProject.asgi.application'   # added to configure the channels
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
